3cd6fd5078d2d570dfda06dea408df2df022a9e1
donmahallem <donmahallem@users.noreply.github.com>
2025-01-01 20:53:26 +0100

    scale player to window min size

--- base_game.py
+++ base_game.py
@@ -1,13 +1,17 @@
 import pygame
 from player import Player
 from OpenGL import GL
+from constants import PIXEL_SIZE_HORIZONTAL, PIXEL_SIZE_VERTICAL
 
 
 class BaseGame(pygame.sprite.Group):
     def __init__(self, width, height, *sprites):
         super().__init__(*sprites)
         self.rect = pygame.rect.Rect(0, 0, width, height)
-        self.player = Player(50, 75, 20, 30, pygame.rect.Rect(0, 0, width, height))
+        player_size = min(PIXEL_SIZE_HORIZONTAL, PIXEL_SIZE_VERTICAL)
+        self.player = Player(
+            50, 75, player_size, player_size, pygame.rect.Rect(0, 0, width, height)
+        )
         self.add_internal(self.player)
         self.player.velocity.update(300, 30)
 
